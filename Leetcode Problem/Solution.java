5aa18804e3153c82414eb8495b3757f9d10153ac
= <=>
2025-01-19 09:38:37 +0530

    new one

---
+++ Leetcode Problem/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i =1 ;
+        for(int j = 1;j<nums.length;j++)
+        {
+            if(nums[i]!=nums[j])
+            {
+                nums[i+1]=nums[j];
+                i++;
+             }
+        }
+        return i;
+    }
+} 
+    
+}
