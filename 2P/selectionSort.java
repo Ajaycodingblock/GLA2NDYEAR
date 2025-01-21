c1a2326ef878e751e9cfd1d68f2c05962bcb77c8
= <=>
2025-01-21 13:13:25 +0530

    new one

--- 2P/selectionSort.java
+++ 2P/selectionSort.java
@@ -1,46 +1,26 @@
 public class selectionSort {
 
-
-    public static void selectsort(int [] arr)
-    {
-
+     public static void selctsort(int [] arr)
+     {
         int n = arr.length;
-for(int i =0 ;i<n-1;i++)
+for (int i =0; i<n-1;i++)
 {
-    int mx = i;
-    for(int j =i;j<n;j++)
+    int mx = i ;
+    for (int j =i ;j<n;j++)
     {
-        
-        if(arr[j]<arr[mx])
+        if(arr[mx]>arr[j])
         {
-           mx = j;
+            mx = j;
         }
-
-    }
-    int temp = arr[i];
-    arr[i] = arr[mx]; 
-    arr[mx] =  temp;
-
-}
-    }
-
-
-
+    }       int temp = arr[i];
+            arr[i]=arr[mx];
+            arr[mx]=temp;
+}    }
     public static void main(String[] args) {
-        int arr[] = {2,4,1,5,3,6};
-
+        int [] arr ={2,4,1,5,6,3};
+        selctsort(arr);
         for(int itr : arr)
-        {
-            System.out.print(itr + " ");
+        {System.out.print(itr + " ");
         }
-        System.out.println();
-
-        selectsort(arr);
-
-        for(int itr : arr)
-        {
-            System.out.print(itr + " ");
-        }
-
     }
 }
