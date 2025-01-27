c9026e0319fadb0e1e4dab0220e685c9c6e0d4c2
= <=>
2025-01-27 13:29:59 +0530

    neew

---
+++ 2V/Lecture_4/Bubblesort.java
@@ -0,0 +1,40 @@
+public class Bubblesort {
+
+    // Method to perform Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        // Traverse through all elements in the array
+        for (int i = 0; i < n - 1; i++) {
+            // Last i elements are already sorted
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Unsorted array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
