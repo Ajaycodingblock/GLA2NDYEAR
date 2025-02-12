03dcb5520a789b4d1acfb22ac92252577ac7baf3
= <=>
2025-02-12 09:50:39 +0530

    Linked list

---
+++ 2P/Lecture_12/LinkedList.java
@@ -0,0 +1,24 @@
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+
+
+public class LinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(12);
+        System.out.println(head.data);
+        
+        
+    }
+}
