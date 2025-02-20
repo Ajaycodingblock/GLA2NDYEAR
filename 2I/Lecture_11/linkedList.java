599e2ab3733f3eb3ed44a32334a3a06daa432eef
= <=>
2025-02-20 14:44:10 +0530

    LL

---
+++ 2I/Lecture_11/linkedList.java
@@ -0,0 +1,26 @@
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+
+
+public class LinkedList {
+    public static void main(String[] args) {
+        
+        Node head = new Node(12);
+
+        System.out.println(head.data);
+        System.out.println(head.next);
+        
+    }
+}
