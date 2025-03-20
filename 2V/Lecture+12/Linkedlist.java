7534a56d93e12e67e4ed491def8864b4e2c793c7
= <=>
2025-03-20 20:13:36 +0530

    Linked List

---
+++ 2V/Lecture+12/Linkedlist.java
@@ -0,0 +1,57 @@
+class ListNode
+{
+    int val;
+    ListNode next;
+    
+ public ListNode(int x)
+ {
+     val = x;
+     next = null;
+ }
+    
+}
+
+class Linkedlist {
+
+  public static void main(String[] args) {
+      
+      ListNode node1 = new ListNode(1);
+      ListNode node2 = new ListNode(2);
+      ListNode node3 = new ListNode(3);
+      
+      node1.next = node2;
+      node2.next = node3;
+      
+      System.out.println("Node addresses:");
+      
+      System.out.println(node1);
+      System.out.println(node2);
+      System.out.println(node3);
+      
+     ListNode ptr = node1;
+     
+    System.out.println("ptr value:");
+     
+     while(ptr!=null)
+     {
+         System.out.println(ptr.val);
+         ptr = ptr.next;
+         
+     }
+     
+     System.out.println("next values");
+     
+     
+     ptr = node1;
+     
+     while(ptr!=null)
+     {
+         System.out.println(ptr.next);
+         ptr = ptr.next;
+         
+     }
+      
+      
+    
+  }
+}
---
+++ 2V/Lecture_13/LLOpertaion.java
@@ -0,0 +1,97 @@
+ class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x, ListNode add) {
+        val = x;
+        next = add;
+    }
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LLOpertaion {
+
+    public static ListNode insertAtBeginning(ListNode head, int data) {
+        ListNode newNode = new ListNode(data);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    public static ListNode insertAtEnd(ListNode head, int data) {
+        ListNode newNode = new ListNode(data);
+        ListNode ptr = head;
+        while (ptr.next != null)
+            ptr = ptr.next;
+        ptr.next = newNode;
+        return head;
+    }
+
+    public static ListNode insertInMiddle(ListNode head, int data, int position) {
+        ListNode newNode = new ListNode(data);
+        ListNode ptr = head;
+        for (int i = 0; i < position; i++) {
+            ptr = ptr.next;
+        }
+        newNode.next = ptr.next;
+        ptr.next = newNode;
+        return head;
+    }
+
+    public static ListNode deleteFromBeginning(ListNode head) {
+        head = head.next;
+        return head;
+    }
+
+    public static ListNode deleteAtEnd(ListNode head) {
+        ListNode ptr = head;
+        while (ptr.next.next != null)
+            ptr = ptr.next;
+        ptr.next = null;
+        return head;
+    }
+
+    public static ListNode deleteFromMiddle(ListNode head, int position) {
+        ListNode ptr = head;
+        for (int i = 0; i < position - 1; i++) {
+            ptr = ptr.next;
+        }
+        ListNode nodeToDelete = ptr.next;
+        ListNode nextNode = nodeToDelete.next;
+        ptr.next = nextNode;
+        return head;
+    }
+
+    public static void traverseALinkedList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(1);
+        
+
+
+        System.out.println("Original Linked List :- ");
+        traverseALinkedList(node1);
+
+        node1 = insertAtBeginning(node1, 0);  
+        System.out.println("Linked List after inserting 0 at the beginning :- ");
+        traverseALinkedList(node1);
+
+        node1 = insertAtEnd(node1, 2);
+        System.out.println("Linked List after inserting 2 at Last :- ");
+        traverseALinkedList(node1);
+
+
+    }
+}
+
